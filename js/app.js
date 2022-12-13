353b19e0836782ec0dcdb078c0e9f656410698f8
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-12-13 19:38:00 +0100

    keep going

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var ChangeScreen = false;
+
 
 function ChangeVideo(pName){
     let backgroundVideo = document.getElementById('backgroundvideo');
@@ -10,4 +12,24 @@ function AssignAnimation(pVideo){
     let clone = pVideo.cloneNode(true);
     pVideo.parentNode.replaceChild(clone, pVideo);
     // clone.load();
+}
+
+function PauseVideo(){
+    let currentVideo = document.getElementById('videoShowed');
+    currentVideo.pause();
+}
+
+function PlayVideo(){
+    let currentVideo = document.getElementById('videoShowed');
+    currentVideo.play();
+}
+
+function FullScreen(pChanging){
+    let currentVideo = document.getElementById('videoShowed');
+    if(pChanging == false){
+        currentVideo.setAttribute('class', 'back-video fullscreen');
+        pChanging = true;
+    }else{
+        currentVideo.setAttribute('class', 'back-video');
+    }
 }
