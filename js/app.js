27097fd3ca41518551b62efc4e291c5a4e8e64f6
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-01-19 21:09:50 +0100

    update stuff

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var ChangeScreen = false;
 var AudioActivated = false;
 var currentVideo = document.getElementById('videoShowed')
+var carousel = 'img/carousel_madoka'
 
 
 function ChangeVideo(pName){
@@ -46,4 +47,48 @@ function Audio(){
         currentVideo.muted = true;
         AudioActivated = false;
     }
+}
+
+function changeRoute(pRoute){
+    carousel = pRoute
+}
+
+function CarouselControl(pRuta){
+
+
+    let PADRE_CARROUSEL = document.getElementById('carouselExampleControls')
+    PADRE_CARROUSEL.innerHTML = ''
+    let carousel_inner = document.createElement('div');
+    carousel_inner.setAttribute('class', 'carousel-inner');
+    let carousel_item;
+    let img;
+    let array = ['First', 'Second', 'Third']
+    for(let i = 0; i< 2; i++){
+    carousel_item = document.createElement('div');
+    carousel_item.setAttribute('class', 'carousel-item active');
+    img = document.createElement('img')
+    img.setAttribute('alt', `${array[i]} slide`)
+    img.setAttribute('src', `${pRuta}/${i}.jpg`)
+    carousel_item.appendChild(img)
+    carousel_inner.appendChild(carousel_item)
+    }
+
+    PADRE_CARROUSEL.appendChild(carousel_inner);
+
+    
+
+
+
+
+//     <div class="carousel-inner">
+//     <div class="carousel-item active">
+//       <img class="" src="img/chara1_img.png" alt="First slide">
+//     </div>
+//     <div class="carousel-item">
+//       <img class="" src="img/chara2_img.png" alt="Second slide">
+//     </div>
+//     <div class="carousel-item">
+//       <img class="" src="img/chara3_img.png" alt="Third slide">
+//     </div>
+//   </div>
 }
